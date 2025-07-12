ebab072f2d7612a31e25126eb0339cf7c3648ad9
nguyenhuuanhdev <nguyenhuuanhdev@gmail.com>
2025-07-13 00:09:30 +0700

    Mô tả thay đổi

--- script.js
+++ script.js
@@ -436,7 +436,7 @@ toggleBtn.addEventListener('click', () => {
 function createHeart() {
     const heart = document.createElement('div');
     heart.classList.add('heart');
-    heart.innerText = '😴';
+    heart.innerText = '💗';
     heart.style.left = Math.random() * 100 + 'vw';
     heart.style.animationDuration = (2 + Math.random() * 3) + 's';
     document.body.appendChild(heart);

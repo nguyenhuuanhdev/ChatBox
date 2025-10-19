87d6d254bdf2d6f535e309bd59ea012ccf813096
nguyenhuuanhdev <nguyenhuuanhdev@gmail.com>
2025-10-19 21:58:05 +0700

    Mô tả thay đổi

--- script.js
+++ script.js
@@ -715,9 +715,9 @@ function startHeartRainSequence() {
 function updateIconByTime() {
     const hour = new Date().getHours();
     if (hour >= 23 || hour < 6) {
-        currentIcon = '🔓';
+        currentIcon = '🌷';
     } else {
-        currentIcon = '🥺';
+        currentIcon = '🌷';
     }
 }
 
